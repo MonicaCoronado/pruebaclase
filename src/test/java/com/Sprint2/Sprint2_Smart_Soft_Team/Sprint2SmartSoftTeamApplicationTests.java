301075b69ca59cd2e526ca8237db856e8db838f6
Juan_Colorado <monicoronado@hotmail.com>
2022-08-27 16:18:12 -0500

    Se realiza primer commit

---
+++ src/test/java/com/Sprint2/Sprint2_Smart_Soft_Team/Sprint2SmartSoftTeamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Sprint2.Sprint2_Smart_Soft_Team;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sprint2SmartSoftTeamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
